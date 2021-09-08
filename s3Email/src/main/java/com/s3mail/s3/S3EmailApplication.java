b78c28644a5c4099664ff99c18d54141b0143510
Rahul Pagidala <Rahul.Pagidala@cashe.local>
2021-09-08 15:33:09 +0530

    Upload,delete,download to S3

---
+++ s3Email/src/main/java/com/s3mail/s3/S3EmailApplication.java
@@ -0,0 +1,13 @@
+package com.s3mail.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3EmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3EmailApplication.class, args);
+	}
+
+}
